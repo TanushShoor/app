831a00aee13724874d3dcae2b6279f1b38ad7749
Tanush Shoor <shoortanush992005@gmail.com>
2025-04-03 07:55:46 +0000

    re-edited part-2

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ bt.addEventListener('click', ()=>{
 // const supabase = createClient('https://fxupgkzprrbkzapufdpu.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ4dXBna3pwcnJia3phcHVmZHB1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMwMTUyODIsImV4cCI6MjA1ODU5MTI4Mn0.CCaUd4sqk_NN9N19-pglJa-B4JiSZTTVR_Mwj4pMcWk');
 
 
-import { createClient } from '@supabase/supabase-js'
+// import { createClient } from '@supabase/supabase-js'
+const createClient = require('@supabase/supabase-js');
 const supabaseUrl = 'https://fxupgkzprrbkzapufdpu.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ4dXBna3pwcnJia3phcHVmZHB1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMwMTUyODIsImV4cCI6MjA1ODU5MTI4Mn0.CCaUd4sqk_NN9N19-pglJa-B4JiSZTTVR_Mwj4pMcWk';
 const supabase = createClient(supabaseUrl, supabaseKey)
